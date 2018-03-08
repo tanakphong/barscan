c228a4a9e1aa21a63188b19a71ebfd232f62ebd3
tanakphong <t.phongsing@gmail.com>
2018-03-08 15:13:04 +0700

    Remove Button change view 1D & 2D

--- app/src/main/java/com/deverdie/barscan/CustomScannerActivity.java
+++ app/src/main/java/com/deverdie/barscan/CustomScannerActivity.java
@@ -9,7 +9,6 @@ import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.Toast;
 
 import com.journeyapps.barcodescanner.CaptureManager;
 import com.journeyapps.barcodescanner.DecoratedBarcodeView;
@@ -24,7 +23,6 @@ public class CustomScannerActivity extends Activity implements
     private CaptureManager capture;
     private DecoratedBarcodeView barcodeScannerView;
     private ImageView mImgFlash;
-    private ImageView mImg1D;
     private Button mBtnChangeOrientation;
     private boolean isPortrait = true;
     private static final String IS_PORTRAIT = "IS_ID";
@@ -43,23 +41,9 @@ public class CustomScannerActivity extends Activity implements
         barcodeScannerView =  findViewById(R.id.zxing_barcode_scanner);
         barcodeScannerView.setTorchListener(this);
 
-//        barcodeSurface = (BarcodeView) findViewById(R.id.zxing_barcode_surface);
-//        barcodeSurface.setFramingRectSize(new Size(400,100));
-//        if (is1D) {
-//            barcodeScannerView.getBarcodeView().setFramingRectSize(new Size(400, 100));
-//        } else {
-//            barcodeScannerView.getBarcodeView().setFramingRectSize(new Size(300, 300));
-//        }
-
-
         mImgFlash =  findViewById(R.id.imgFlash);
         mImgFlash.setImageResource(R.drawable.flash_on);
         mImgFlash.setTag("on");
-
-        mImg1D =  findViewById(R.id.img1D);
-        mImg1D.setImageResource(R.drawable.flash_on);
-        mImg1D.setTag("1d");
-        mImg1D.setVisibility(View.GONE);
 
         Log.i(TAG, "onCreate: "+isPortrait);
         mBtnChangeOrientation = findViewById(R.id.change_orientation);
@@ -84,48 +68,6 @@ public class CustomScannerActivity extends Activity implements
             }
         });
 
-        mImg1D.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (String.valueOf(mImg1D.getTag()).equals("1d")) {
-//                    barcodeSurface.setFramingRectSize(new Size(400,100));
-//                    barcodeScannerView.getBarcodeView().removeAllViews();
-//                    DisplayMetrics metrics = new DisplayMetrics();
-//                    getWindowManager().getDefaultDisplay().getMetrics(metrics);
-//                    int width = metrics.widthPixels;
-//                    int height = metrics.heightPixels;
-//                    barcodeScannerView.getBarcodeView().setFramingRectSize(new Size((int) (width*0.9f), (int) (height*0.25f)));
-//                    RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(
-//                            RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
-//
-//                    layoutParams.setMargins(0, (int) (-1*(height*0.2)), 0, 0);
-//                    barcodeScannerView.setLayoutParams(layoutParams);
-
-//                    barcodeScannerView.getBarcodeView().setFramingRectSize(new Size(400,100));
-//                    CameraSettings cameraSettings = barcodeScannerView.getBarcodeView().getCameraSettings();
-//                    //cameraSettings.setBarcodeSceneModeEnabled(true);
-//                    cameraSettings.setContinuousFocusEnabled(true);
-//                    cameraSettings.setAutoFocusEnabled(true);
-//                    barcodeScannerView.getBarcodeView().setCameraSettings(cameraSettings);
-//                    is1D = true;
-//                    mImg1D.setTag("2d");
-//                    mImg1D.setImageResource(R.drawable.flash_off);
-                    Toast.makeText(getApplicationContext(), "1D", Toast.LENGTH_SHORT).show();
-                } else {
-//                    barcodeSurface.setFramingRectSize(new Size(200,200));
-//                    barcodeScannerView.getBarcodeView().setFramingRectSize(new Size(300,300));
-//                    barcodeScannerView.getBarcodeView().refreshDrawableState();
-//                    mImg1D.setTag("1d");
-//                    mImg1D.setImageResource(R.drawable.flash_off);
-//                    is1D = false;
-                    Toast.makeText(getApplicationContext(), "2D", Toast.LENGTH_SHORT).show();
-                }
-                finish();
-
-                startActivity(getIntent());
-            }
-        });
-
         mBtnChangeOrientation.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -135,13 +77,6 @@ public class CustomScannerActivity extends Activity implements
                 }else{
                     setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
                 }
-//                if (String.valueOf(mBtnChangeOrientation.getTag()).equals("portrait")) {
-//                    setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
-//                    isPortrait = true;
-//                } else if (String.valueOf(mBtnChangeOrientation.getTag()).equals("landscape")) {
-//                    setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
-//                    isPortrait = false;
-//                }
             }
         });
 
