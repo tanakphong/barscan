9b539557f7541b35b1f661a84b1dc6a3dabb7c78
tanakphong <t.phongsing@gmail.com>
2018-03-08 14:59:19 +0700

    Fix bug It can change orientation scannerviewer.

--- app/src/main/java/com/deverdie/barscan/CustomScannerActivity.java
+++ app/src/main/java/com/deverdie/barscan/CustomScannerActivity.java
@@ -1,15 +1,19 @@
 package com.deverdie.barscan;
 
 import android.app.Activity;
+import android.content.pm.ActivityInfo;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.journeyapps.barcodescanner.CaptureManager;
 import com.journeyapps.barcodescanner.DecoratedBarcodeView;
+import com.journeyapps.barcodescanner.Size;
 
 /**
  * Custom Scannner Activity extending from Activity to display a custom layout form scanner view.
@@ -19,49 +23,130 @@ public class CustomScannerActivity extends Activity implements
 
     private CaptureManager capture;
     private DecoratedBarcodeView barcodeScannerView;
-    private Button switchFlashlightButton;
-    private ImageButton mImgFlash;
+    private ImageView mImgFlash;
+    private ImageView mImg1D;
+    private Button mBtnChangeOrientation;
+    private boolean isPortrait = true;
+    private static final String IS_PORTRAIT = "IS_ID";
+    private static final String TAG = "dlg";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_custom_scanner);
 
-        barcodeScannerView = (DecoratedBarcodeView)findViewById(R.id.zxing_barcode_scanner);
+        if (savedInstanceState != null) {
+            isPortrait = savedInstanceState.getBoolean(IS_PORTRAIT);
+            Log.i(TAG, "savedInstanceState IS_PORTRAIT: "+isPortrait);
+        }
+
+        barcodeScannerView =  findViewById(R.id.zxing_barcode_scanner);
         barcodeScannerView.setTorchListener(this);
 
-        switchFlashlightButton = (Button)findViewById(R.id.switch_flashlight);
-        switchFlashlightButton.setVisibility(View.INVISIBLE);
-        mImgFlash = (ImageButton) findViewById(R.id.imgFlash);
+//        barcodeSurface = (BarcodeView) findViewById(R.id.zxing_barcode_surface);
+//        barcodeSurface.setFramingRectSize(new Size(400,100));
+//        if (is1D) {
+//            barcodeScannerView.getBarcodeView().setFramingRectSize(new Size(400, 100));
+//        } else {
+//            barcodeScannerView.getBarcodeView().setFramingRectSize(new Size(300, 300));
+//        }
+
+
+        mImgFlash =  findViewById(R.id.imgFlash);
         mImgFlash.setImageResource(R.drawable.flash_on);
         mImgFlash.setTag("on");
-        //mImgFlash.setVisibility(View.INVISIBLE);
+
+        mImg1D =  findViewById(R.id.img1D);
+        mImg1D.setImageResource(R.drawable.flash_on);
+        mImg1D.setTag("1d");
+        mImg1D.setVisibility(View.GONE);
+
+        Log.i(TAG, "onCreate: "+isPortrait);
+        mBtnChangeOrientation = findViewById(R.id.change_orientation);
+        if (isPortrait) {
+            barcodeScannerView.getBarcodeView().setFramingRectSize(new Size(400, 100));
+            mBtnChangeOrientation.setTag("landscape");
+            mBtnChangeOrientation.setText(getResources().getString(R.string.landscape));
+        }else{
+            barcodeScannerView.getBarcodeView().setFramingRectSize(new Size(300, 300));
+            mBtnChangeOrientation.setTag("portrait");
+            mBtnChangeOrientation.setText(getResources().getString(R.string.portrait));
+        }
+
         mImgFlash.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //Toast.makeText(CustomScannerActivity.this, String.valueOf(mImgFlash.getTag()), Toast.LENGTH_SHORT).show();
-                if(String.valueOf(mImgFlash.getTag())=="on"){
+                if (String.valueOf(mImgFlash.getTag()).equals("on")) {
                     barcodeScannerView.setTorchOn();
-                    //Toast.makeText(CustomScannerActivity.this, "on", Toast.LENGTH_SHORT).show();
-                }else{
+                } else {
                     barcodeScannerView.setTorchOff();
-                    //Toast.makeText(CustomScannerActivity.this, "off", Toast.LENGTH_SHORT).show();
                 }
             }
         });
 
-        // if the device does not have flashlight in its camera,
-        // then remove the switch flashlight button...
-        if (!hasFlash()) {
-            //switchFlashlightButton.setVisibility(View.GONE);
-            mImgFlash.setVisibility(View.GONE);
-            //mImgFlash.setImageResource(R.drawable.flash_on);
+        mImg1D.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (String.valueOf(mImg1D.getTag()).equals("1d")) {
+//                    barcodeSurface.setFramingRectSize(new Size(400,100));
+//                    barcodeScannerView.getBarcodeView().removeAllViews();
+//                    DisplayMetrics metrics = new DisplayMetrics();
+//                    getWindowManager().getDefaultDisplay().getMetrics(metrics);
+//                    int width = metrics.widthPixels;
+//                    int height = metrics.heightPixels;
+//                    barcodeScannerView.getBarcodeView().setFramingRectSize(new Size((int) (width*0.9f), (int) (height*0.25f)));
+//                    RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(
+//                            RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+//
+//                    layoutParams.setMargins(0, (int) (-1*(height*0.2)), 0, 0);
+//                    barcodeScannerView.setLayoutParams(layoutParams);
 
-//            if(mImgFlash.getVisibility()==View.VISIBLE){
-//                mImgFlash.setVisibility(View.GONE);
-//            }else{
-//                mImgFlash.setVisibility(View.VISIBLE);
-//            }
+//                    barcodeScannerView.getBarcodeView().setFramingRectSize(new Size(400,100));
+//                    CameraSettings cameraSettings = barcodeScannerView.getBarcodeView().getCameraSettings();
+//                    //cameraSettings.setBarcodeSceneModeEnabled(true);
+//                    cameraSettings.setContinuousFocusEnabled(true);
+//                    cameraSettings.setAutoFocusEnabled(true);
+//                    barcodeScannerView.getBarcodeView().setCameraSettings(cameraSettings);
+//                    is1D = true;
+//                    mImg1D.setTag("2d");
+//                    mImg1D.setImageResource(R.drawable.flash_off);
+                    Toast.makeText(getApplicationContext(), "1D", Toast.LENGTH_SHORT).show();
+                } else {
+//                    barcodeSurface.setFramingRectSize(new Size(200,200));
+//                    barcodeScannerView.getBarcodeView().setFramingRectSize(new Size(300,300));
+//                    barcodeScannerView.getBarcodeView().refreshDrawableState();
+//                    mImg1D.setTag("1d");
+//                    mImg1D.setImageResource(R.drawable.flash_off);
+//                    is1D = false;
+                    Toast.makeText(getApplicationContext(), "2D", Toast.LENGTH_SHORT).show();
+                }
+                finish();
+
+                startActivity(getIntent());
+            }
+        });
+
+        mBtnChangeOrientation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                isPortrait = !isPortrait;
+                if (isPortrait) {
+                    setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+                }else{
+                    setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+                }
+//                if (String.valueOf(mBtnChangeOrientation.getTag()).equals("portrait")) {
+//                    setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+//                    isPortrait = true;
+//                } else if (String.valueOf(mBtnChangeOrientation.getTag()).equals("landscape")) {
+//                    setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+//                    isPortrait = false;
+//                }
+            }
+        });
+
+        if (!hasFlash()) {
+            mImgFlash.setVisibility(View.GONE);
         }
 
         capture = new CaptureManager(this, barcodeScannerView);
@@ -91,6 +176,7 @@ public class CustomScannerActivity extends Activity implements
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
         capture.onSaveInstanceState(outState);
+        outState.putBoolean(IS_PORTRAIT, isPortrait);
     }
 
     @Override
@@ -98,34 +184,19 @@ public class CustomScannerActivity extends Activity implements
         return barcodeScannerView.onKeyDown(keyCode, event) || super.onKeyDown(keyCode, event);
     }
 
-    /**
-     * Check if the device's camera has a Flashlight.
-     * @return true if there is Flashlight, otherwise false.
-     */
     private boolean hasFlash() {
         return getApplicationContext().getPackageManager()
                 .hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH);
     }
 
-    public void switchFlashlight(View view) {
-        if (getString(R.string.turn_on_flashlight).equals(switchFlashlightButton.getText())) {
-            barcodeScannerView.setTorchOn();
-        } else {
-            barcodeScannerView.setTorchOff();
-        }
-
-    }
-
     @Override
     public void onTorchOn() {
-        switchFlashlightButton.setText(R.string.turn_off_flashlight);
         mImgFlash.setTag("off");
         mImgFlash.setImageResource(R.drawable.flash_off);
     }
 
     @Override
     public void onTorchOff() {
-        switchFlashlightButton.setText(R.string.turn_on_flashlight);
         mImgFlash.setTag("on");
         mImgFlash.setImageResource(R.drawable.flash_on);
     }
--- app/src/main/java/com/deverdie/barscan/MainActivity.java
+++ app/src/main/java/com/deverdie/barscan/MainActivity.java
@@ -5,14 +5,13 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.gms.ads.AdListener;
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
 import com.google.android.gms.ads.MobileAds;
@@ -22,7 +21,7 @@ import com.google.zxing.integration.android.IntentResult;
 public class MainActivity extends AppCompatActivity {
 
     private TextView mTxtValue;
-    private Button mBtnScan;
+    private ImageView mBtnScan;
     private LinearLayout mLayControl;
     private Button mBtnCopyToClipboard;
     private Button mBtnLinkTo;
@@ -33,101 +32,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-//        MobileAds.initialize(this, "ca-app-pub-3971225854562108/5590847389");
-//        NativeExpressAdView adView = (NativeExpressAdView)findViewById(R.id.adView);
-//
-//        AdRequest request = new AdRequest.Builder().build();
-//        adView.loadAd(request);
-
-//        AdRequest.Builder adBuilder = new AdRequest.Builder();
-//        AdRequest adRequest = adBuilder.build();
-//        AdView adView = (AdView)findViewById(R.id.adView);
-//        adView.loadAd(adRequest);
-
-//        AdView adView = (AdView) findViewById(R.id.adView);
-//        AdRequest adRequest = new AdRequest.Builder()
-//                .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
-//                .build();
-//        adView.loadAd(adRequest);
-
-//        mAdView = (AdView) findViewById(R.id.adView);
-//        AdRequest adRequest = new AdRequest.Builder().build();
-//        mAdView.loadAd(adRequest);
-
-//        MobileAds.initialize(getApplicationContext(),
-//                "ca-app-pub-3971225854562108/5590847389");
-//
-//        mAdView = (AdView) findViewById(R.id.adView);
-//        AdRequest adRequest = new AdRequest.Builder().build();
-//        mAdView.loadAd(adRequest);
-
-        // Initialize the Mobile Ads SDK.ca-app-pub-3971225854562108~5106343033
         MobileAds.initialize(this, "ca-app-pub-3971225854562108~9382930342");
-        //MobileAds.initialize(this, "ca-app-pub-3940256099942544/6300978111");
-
-        // Gets the ad view defined in layout/ad_fragment.xml with ad unit ID set in
-        // values/strings.xml.
-        mAdView = (AdView) findViewById(R.id.ad_view);
-
-        // Create an ad request. Check your logcat output for the hashed device ID to
-        // get test ads on a physical device. e.g.
-        // "Use AdRequest.Builder.addTestDevice("ABCDEF012345") to get test ads on this device."
+        mAdView =  findViewById(R.id.ad_view);
         AdRequest adRequest = new AdRequest.Builder()
                 .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
                 .build();
-
-        // Start loading the ad in the background.
         mAdView.loadAd(adRequest);
 
-        mAdView.setAdListener(new AdListener() {
-            @Override
-            public void onAdLoaded() {
-                // Code to be executed when an ad finishes loading.
-                Log.i("Ads", "onAdLoaded");
-            }
-
-            @Override
-            public void onAdFailedToLoad(int errorCode) {
-                // Code to be executed when an ad request fails.
-                Log.i("Ads", "onAdFailedToLoad");
-            }
-
-            @Override
-            public void onAdOpened() {
-                // Code to be executed when an ad opens an overlay that
-                // covers the screen.
-                Log.i("Ads", "onAdOpened");
-            }
-
-            @Override
-            public void onAdLeftApplication() {
-                // Code to be executed when the user has left the app.
-                Log.i("Ads", "onAdLeftApplication");
-            }
-
-            @Override
-            public void onAdClosed() {
-                // Code to be executed when when the user is about to return
-                // to the app after tapping on an ad.
-                Log.i("Ads", "onAdClosed");
-            }
-        });
-
-//        final Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-//        toolbar.setTitle(getResources().getString(R.string.title_activity_main));
-//        toolbar.setTitleTextColor(Color.WHITE);
-//        if (toolbar != null) {
-//            setSupportActionBar(toolbar);
-//        }
-
-        mTxtValue = (TextView) findViewById(R.id.txtValue);
-        mBtnScan = (Button) findViewById(R.id.btnScan);
-        mLayControl = (LinearLayout) findViewById(R.id.layControl);
-        mLayControl.setVisibility(View.INVISIBLE);
-        mBtnCopyToClipboard = (Button) findViewById(R.id.btnCopyToClipboard);
-        mBtnLinkTo = (Button) findViewById(R.id.btnLinkto);
-        mBtnGoTo = (Button) findViewById(R.id.btnGoto);
+        mTxtValue =  findViewById(R.id.txtValue);
+        mBtnScan =  findViewById(R.id.imgScan);
+        mLayControl =  findViewById(R.id.layControl);
+        mLayControl.setVisibility(View.GONE);
+        mBtnCopyToClipboard =  findViewById(R.id.btnCopyToClipboard);
+        mBtnLinkTo =  findViewById(R.id.btnLinkto);
+        mBtnGoTo =  findViewById(R.id.btnGoto);
 
         mBtnCopyToClipboard.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -154,11 +72,15 @@ public class MainActivity extends AppCompatActivity {
         mBtnGoTo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String val = mTxtValue.getText().toString();
-                Uri uri = Uri.parse(val);
-                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
-                startActivity(intent);
-//                Toast.makeText(MainActivity.this, "Link to", Toast.LENGTH_SHORT).show();
+                try{
+                    String val = mTxtValue.getText().toString();
+                    Uri uri = Uri.parse(val);
+                    Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                    startActivity(intent);
+                }catch (Exception e){
+                    Toast.makeText(MainActivity.this, getResources().getString(R.string.format_invalid), Toast.LENGTH_SHORT).show();
+                    e.printStackTrace();
+                }
             }
         });
 
@@ -167,7 +89,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 mTxtValue.setText("");
-                mLayControl.setVisibility(View.INVISIBLE);
+                mLayControl.setVisibility(View.GONE);
                 IntentIntegrator integrator = new IntentIntegrator(MainActivity.this);
                 integrator.setCaptureActivity(CustomScannerActivity.class);
                 integrator.setPrompt(getResources().getString(R.string.scan_help));
@@ -185,9 +107,8 @@ public class MainActivity extends AppCompatActivity {
             if (result.getContents() == null) {
                 Toast.makeText(this, getResources().getString(R.string.cancelled), Toast.LENGTH_LONG).show();
             } else {
-                mTxtValue.setText(result.getContents().toString());
+                mTxtValue.setText(result.getContents());
                 mLayControl.setVisibility(View.VISIBLE);
-
             }
         } else {
             super.onActivityResult(requestCode, resultCode, data);
